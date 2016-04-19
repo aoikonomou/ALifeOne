2b6e1f92128b63e09d9929a49645f959ca45c3f3
andreasoikonomou <andreas.oikonomou@ntu.ac.uk>
2016-04-20 00:30:17 +0100

    Some code clean up and very basic initial functionality (i.e. setting canvas size and printing circles). Need to add customisation functionality to what is printed next.

--- test.js
+++ test.js
@@ -1,155 +1,111 @@
 /**
  * Created by andreasoikonomou on 16/04/2016.
  */
+
+// The first version of this program creates random, custom size, patterned backgrounds
+
+
 //Global var
 var stage;
 var image;
 var shapeArray;
 
-var stageWidth = 1800;
-var stageHeight = 1000;
+var canvasWidth;
+var canvasHeight;
+
+// Initialise environment
 
 function init() {
 
-    stage = new createjs.Stage("demoCanvas");
 
-    image = new createjs.Bitmap("img/gear.png");
-    image.regX =image.width/2; // registration point?
-    image.regY=image.height/2;
-    stage.addChild(image);
+    //canvasWidth=document.getElementById("demoCanvas").width;
+    //canvasHeight=document.getElementById("demoCanvas").height;
+    //
+    stage = new createjs.Stage("demoCanvas"); // Bind the createjs library to the html canvas
+    //
+    //image = new createjs.Bitmap("img/gear.png"); // Load an image resource into this variable
+    //image.regX = image.width / 2; // registration point?
+    //image.regY = image.height / 2;
+    //stage.addChild(image); // Add image variable to stage. If not on stage it's not visible
 
 
     shapeArray = [];
-    var a = createjs.Ticker.addEventListener("tick", handleTick);
-    a.framerate = 3;
 
-    myLoop(stage, 60, stageWidth, stageHeight, shapeArray);
-    setCanvasBackground();
+
+    // Create graphics
+    var type="circle";
+    var numberOfObjects=50; // How many objects do you want;
+    var minSize=10; // Minimum graphic size
+    var maxSize=100; // Maximum graphic size
+    var color=randomHexColor(); // Random colour from random colour function
+    var xPos=randomInteger(0,canvasWidth); // Graphic position somewhere withing the screen bounds
+    var yPos=randomInteger(0,canvasWidth); // Graphic position somewhere withing the screen bounds
+
+
+  // createGraphics(type, numberOfObjects, minSize, maxSize, color, xPos, yPos); // Call createGraphics function with relevant parameters
+
+    message("I did the init");
 
 }
 
-function circle(stage, posX, posY, radius, color, shapeArray) {
+function setupCanvasByUser() {
+
+    // Get current values from form fields
+    canvasWidth = document.getElementById("canvasWidth").value;
+    canvasHeight = document.getElementById("canvasHeight").value;
+
+    message("New canvas size is "+canvasWidth +" * " + canvasHeight);
+
+    // Set canvas size to what the user put in the form fields
+    document.getElementById("demoCanvas").width = canvasWidth;
+    document.getElementById("demoCanvas").height = canvasHeight;
+
+    setCanvasBackground();
+
+
+}
+
+function setCanvasBackground(){
+
+    // Change the canvas' background colour (by painting a rectangle in it)
+    var canvas=document.getElementById("demoCanvas");
+    var context=canvas.getContext("2d");
+    context.fillStyle= document.getElementById("canvasBackgroundColor").value;
+    context.fillRect(0,0,canvasWidth,canvasHeight);
+
+}
+
+
+function drawCircles(){
+
+    //Reset the circles array so that it has nothing in it
+    shapeArray=[];
+
+    createGraphics("circle", 50);
+
+}
+
+
+function createGraphics(type,numberOfObjects) {
+
+    for (var i = 1; i < numberOfObjects; i++) {
+        if (type == "circle") {
+            circle(stage, randomInteger(0,canvasWidth), randomInteger(0,canvasHeight), randomInteger(10,100), randomHexColor());
+        }
+    }
+
+}
+
+
+function circle(stage, xPos, yPos, radius, color) {
 
     var circle = new createjs.Shape();
-    circle.graphics.beginFill(color).drawCircle(posX, posY, radius);
-    circle.x = randomInteger(1, 100);
+    circle.graphics.beginFill(color).drawCircle(xPos, yPos, radius);
+    //circle.x = xPos;
+    //circle.y= yPos;
+
     shapeArray.push(circle);
     stage.addChild(circle);
     stage.update();
-
-}
-
-function rectangle(stage, posX, posY, radius, color) {
-
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawRect(posX, posY, radius);
-    circle.x = randomInteger(1, 400);
-    circle.y = randomInteger(1, 400);
-    stage.addChild(circle);
-    stage.update();
-
-}
-
-
-function myLoop(stage, iterations, stageWidth, stageHeight, shapeArray) {
-
-
-    for (var i = 1; i < iterations; i++) {
-
-        circle(stage, randomInteger(5, stageWidth), randomInteger(5, stageHeight), randomInteger(5, 50), randomHexColor(), shapeArray);
-    }
-
-}
-
-
-function randomInteger(min, max) {
-
-    var randomNumber;
-    randomNumber = Math.floor((Math.random() * max) + min);
-
-    console.log(randomNumber.toString(16));
-    return randomNumber;
-
-
-}
-
-function randomHexColor() {
-
-    var randomHexColor;
-    randomHexColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    return randomHexColor;
-
-}
-
-
-/*
-function handleTick(event) {
-
-shapeArray.forEach(function(entry){
-
-    shapeArray[entry].x += 5;
-
-
-})
-console.log(randomInteger(1,100));
-    stage.update();
-}
-*/
-
-
-
-
-function handleTick(event) {
-
-
-
-    image.x += 10;
-    image.rotation +=3;
-
-
-    checkGraphicAgainstBoundaries(image, 0, stageWidth, 0, stageHeight);
-    moveBalls();
-   // console.log(randomInteger(1,100));
-    stage.update();
-}
-
-
-function checkGraphicAgainstBoundaries(graphic,left, right, top, bottom){
-
-    if (graphic.x>= right){
-        graphic.x=left+10;
-    }
-
-    if (graphic.x<= left){
-        graphic.x=right;
-    }
-
-    if (graphic.y>= bottom){
-        graphic.y=top;
-    }
-
-    if (graphic.y>= bottom){
-        graphic.y=top;
-    }
-
-    message("Right="+right);
-
-}
-
-function moveBalls(){
-
-    for(var i=0;i<shapeArray.length;i++){
-
-        shapeArray[i].x += randomInteger(1,20);
-        shapeArray[i].y += randomInteger(1,20);
-
-    }
-
-}
-
-
-function message(text){
-
-    console.log(text);
 
 }
